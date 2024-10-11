1f44483b6b42f341333dc3db4e67b432756c9a2d
jean carlos <jeancarlos3102@gmail.com>
2024-10-11 11:58:25 -0400

    test(Login): add test for loginComponent

--- src/__tests__/Login.test.tsx
+++ src/__tests__/Login.test.tsx
@@ -22,7 +22,9 @@ describe('LoginComponent', () => {
 	test('Show an error when input does not contain @', () => {
 		fireEvent.change(email, { target: { value: 'jeanCarlos.com' } });
 		fireEvent.click(submit);
-		expect(screen.getByText('Please enter a valid email address')).toBeInTheDocument();
+		expect(
+			screen.getByText('Please enter a valid email address')
+		).toBeInTheDocument();
 	});
 
 	test('it should allow typing in the email input', () => {
@@ -30,8 +32,32 @@ describe('LoginComponent', () => {
 		expect(email).toHaveValue('jean@gmail.com');
 	});
 
+	test('Should show an error message when email is empty', () => {
+		fireEvent.change(email, { target: { value: '' } });
+		fireEvent.click(submit);
+		expect(screen.getByText('Email is required')).toBeInTheDocument();
+	});
+
 	test('it should allow typing in the password input', () => {
 		fireEvent.change(password, { target: { value: '123456' } });
 		expect(password).toHaveValue('123456');
+	});
+
+	test('Should show an error message when password is empty', () => {
+		fireEvent.change(email, { target: { value: 'jean@gmail.com' } });
+		fireEvent.change(password, { target: { value: '' } });
+		fireEvent.click(submit);
+		screen.debug();
+		expect(screen.getByText('Password is required')).toBeInTheDocument();
+	});
+
+	test('should not show nay error when form is valid', () => {
+		fireEvent.change(email, { target: { value: 'jean@gmail.com' } });
+		fireEvent.change(password, { target: { value: '123456' } });
+		fireEvent.click(submit);
+
+		expect(screen.queryByText("Email is required")).not.toBeInTheDocument()
+		expect(screen.queryByText("Please enter a valid email address")).not.toBeInTheDocument()
+		expect(screen.queryByText("Password is required")).not.toBeInTheDocument()
 	});
 });
